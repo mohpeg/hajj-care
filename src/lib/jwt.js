5ea7d695ada8a7f6b2982de63591eb14890b98e9
mahmoud13989 <mahmoud13989@gmail.com>
2025-04-26 23:59:50 +0300

    Throw UnauthorizedException for invalid JWT tokens

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
-
+const {
+  UnauthorizedException,
+} = require('../exceptions/unauthorized.exception');
 function generateAccessToken({ accountId, role, hajjId }) {
   return jwt.sign({ sub: accountId, hajjId, role }, process.env.JWT_SECRET, {
     expiresIn: '8h',
@@ -18,7 +20,7 @@ function verifyToken(token) {
   try {
     return jwt.verify(token, process.env.JWT_SECRET);
   } catch (err) {
-    return null;
+    throw new UnauthorizedException('Invalid token');
   }
 }
 
