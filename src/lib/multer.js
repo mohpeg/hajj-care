07bdd9a14586e34df580766c7e6724705042e4bd
mahmoud13989 <mahmoud13989@gmail.com>
2025-04-28 09:16:59 +0300

    Refactor multer configuration for image upload storage and enhance code readability

--- src/lib/multer.js
+++ src/lib/multer.js
@@ -1,16 +1,18 @@
 const multer = require('multer');
 const path = require('path');
 
+// تحديد مكان حفظ الصور
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/passports/');
+    cb(null, 'uploads/passports/'); // احنا نخزن الصور هنا
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     const extension = path.extname(file.originalname);
     cb(null, file.fieldname + '-' + uniqueSuffix + extension);
   },
 });
+
 
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ['.jpeg', '.jpg', '.png'];
