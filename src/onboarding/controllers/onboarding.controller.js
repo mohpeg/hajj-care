d511b8c82f2dae1c5dbfbe2bcbaa3af58d064b59
mahmoud13989 <mahmoud13989@gmail.com>
2025-05-02 22:11:06 +0300

    Refactor onboarding controller to clean up code and add getAllOnboardingData function

--- src/onboarding/controllers/onboarding.controller.js
+++ src/onboarding/controllers/onboarding.controller.js
@@ -1,7 +1,6 @@
 const onboardingService = require('../services/onboarding.service');
 
 async function addHealthConditions(req, res) {
-  console.log(req.user)
   const result = await onboardingService.addHealthConditions(
     req.user.hajjId,
     req.body
@@ -14,15 +13,31 @@ async function addMedicalProcedures(req, res) {
     req.user.hajjId,
     req.body
   );
-  res.status(200).json( req.user.hajjId,result);
+  res.status(200).json(req.user.hajjId, result);
 }
 async function addAllergy(req, res) {
   const result = await onboardingService.addAllergy(req.user.hajjId, req.body);
   res.status(200).json(result);
 }
 
-async function getOnboardingData(req,res) {
+async function getOnboardingData(req, res) {
   const result = await onboardingService.getOnboardingData(req.user.hajjId);
+  res.status(200).json(result);
+}
+
+async function getAllOnboardingData(req, res) {
+  const { pageIndex, pageSize, order } = req.query;
+  const result = await onboardingService.getPaginatedData(
+    pageIndex,
+    pageSize,
+    order
+  );
+  // let { pageIndex, pageSize, order } = req.query;
+  // const result = await onboardingService.getPaginatedData(
+  //   (pageIndex = 1),
+  //   (pageSize = 10),
+  //   (order = 'ASC')
+  // );
   res.status(200).json(result);
 }
 
@@ -31,4 +46,5 @@ module.exports = {
   addMedicalProcedures,
   addAllergy,
   getOnboardingData,
+  getAllOnboardingData,
 };
