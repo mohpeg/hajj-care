ad2d9785124b6e18d9437c36b343b17169017db5
mahmoud13989 <mahmoud13989@gmail.com>
2025-05-14 08:09:17 +0300

    Refactor multer.js for consistent string formatting

--- src/lib/multer.js
+++ src/lib/multer.js
@@ -1,26 +1,25 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 // تحديد مكان حفظ الصور
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/passports/'); // احنا نخزن الصور هنا
+    cb(null, "uploads/passports/"); // احنا نخزن الصور هنا
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const extension = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + extension);
+    cb(null, file.fieldname + "-" + uniqueSuffix + extension);
   },
 });
 
-
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = ['.jpeg', '.jpg', '.png'];
+  const allowedTypes = [".jpeg", ".jpg", ".png"];
   const ext = path.extname(file.originalname).toLowerCase();
   if (allowedTypes.includes(ext)) {
     cb(null, true);
   } else {
-    cb(new Error('Only images (jpeg, jpg, png) are allowed'));
+    cb(new Error("Only images (jpeg, jpg, png) are allowed"));
   }
 };
 
